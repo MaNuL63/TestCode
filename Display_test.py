694d30fb92e77ec09ff21d52a64377daa431895b
MaNuL63 <yaldamanuel9@gmail.com>
2026-04-16 07:39:52 +0200

    Add files via upload

---
+++ Display_test.py
@@ -0,0 +1,45 @@
+import board
+import digitalio
+from adafruit_rgb_display import st7789  
+from PIL import Image, ImageDraw
+
+print("Test mit ST7789 Treiber")
+
+
+spi = board.SPI()
+cs_pin = digitalio.DigitalInOut(board.D6)      
+dc_pin = digitalio.DigitalInOut(board.D24)     
+reset_pin = digitalio.DigitalInOut(board.D25)  
+
+
+disp = st7789.ST7789(
+    spi,
+    rotation=90,     
+    width=240,      
+    height=320,      
+    x_offset=0,      
+    y_offset=0,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=24000000
+)
+
+width = 320
+height = 240
+image = Image.new("RGB", (width, height))
+draw = ImageDraw.Draw(image)
+
+
+draw.rectangle((0, 0, width, height), fill=(0, 255, 0))
+draw.rectangle((0, 0, width-1, height-1), outline=(0, 0, 0), width=10)
+draw.text((width // 2 - 40, height // 2), "ST7789 TEST", fill="black")
+
+
+try:
+
+    disp.image(image)
+    print("geklappt")
+except ValueError:
+    print("kritischer Hase")
+    disp.image(image, rotation=90)
