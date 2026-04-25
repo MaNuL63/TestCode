5702714cacf91a90e78207835d36d0b3ce404037
MaNuL63 <yaldamanuel9@gmail.com>
2026-04-26 01:25:18 +0200

    Update buzzer_test.py

--- buzzer_test.py
+++ buzzer_test.py
@@ -1,23 +1,24 @@
 from gpiozero import Buzzer
 import time
 
-# GPIO pin 
+# Aktiven Buzzer am GPIO-Pin 23 initialisieren
 buzzer = Buzzer(23)
 
 print("Starte Buzzer-Test...")
 print("Du solltest jetzt 3 kurze Pieptöne hören.")
 
 try:
+    # Drei kurze Signaltöne mit jeweils 200ms Dauer ausgeben, die 200ms wurden von meiner uhr gemessen (nicht)
     for i in range(3):
         print(f"Piep {i+1}...")
         buzzer.on()
-        time.sleep(0.2)  
+        time.sleep(0.2)
         buzzer.off()
-        time.sleep(0.2)  
+        time.sleep(0.2)
 
     print("Hat geklappt, ist zu ende")
-    
-    # random auf github gefunden, ist ganz nice eigtl. 
+
+    # 
     while True:
         buzzer.beep(on_time=0.1, off_time=0.5)
         time.sleep(2)
