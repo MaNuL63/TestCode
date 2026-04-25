d953e787edcfb25ceb9d3efccee5a7b62d56459f
MaNuL63 <yaldamanuel9@gmail.com>
2026-04-26 01:20:02 +0200

    Update LED2ColorTest.py
    grün entfernt, da ich die durchgeraucht habe

--- LED2ColorTest.py
+++ LED2ColorTest.py
@@ -1,19 +1,12 @@
 from gpiozero import LED
 import time
 
+# GPIO-Pins fürr die zweifarbige LED, wo ich aber nur die eine Farbe benutze, weil grün mir durchgebrannt ist 
 red = LED(17)
 green = LED(26)
 
-# Rot an
+# Rote LED für eine Sekunde aktivieren
 red.on()
 time.sleep(1)
 red.off()
 
-# Grün an
-green.on()
-time.sleep(5)
-green.off()
-
-# Beide an (ergibt "Yellow")
-red.on()
-green.on()
